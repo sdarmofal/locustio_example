84c9344a26116400fab2d0e610a0f543987622fd
Szymon Darmofał <darmofal.szymon@gmail.com>
2020-01-25 17:15:29 +0100

    get single company

--- api/api.py
+++ api/api.py
@@ -7,3 +7,4 @@ app = falcon.API()
 companies = Companies()
 
 app.add_route('/companies', companies)
+app.add_route('/companies/{company_id:int}', companies, suffix='single')
--- api/endpoints.py
+++ api/endpoints.py
@@ -1,7 +1,7 @@
 import json
 
 from faker import Faker
-from falcon import HTTP_200
+from falcon import HTTP_200, HTTP_404
 
 
 class Companies:
@@ -9,15 +9,25 @@ class Companies:
 
     def __init__(self):
         self.fake = Faker('pl_PL')
-
-    def on_get(self, req, res):
         companies = []
-        for i in range(1, self.COMPANIES_COUNT):
+        for i in range(0, self.COMPANIES_COUNT - 1):
             companies.append({
                 'id': i,
                 'regon': self.fake.regon(),
                 'name': self.fake.company()
             })
+        self.companies = companies
+
+    def on_get(self, req, res):
+        res.status = HTTP_200
+        res.body = json.dumps(self.companies)
+
+    def on_get_single(self, req, res, company_id: int):
+        try:
+            company = self.companies[company_id]
+        except IndexError:
+            res.status = HTTP_404
+            return
 
         res.status = HTTP_200
-        res.body = json.dumps(companies)
+        res.body = json.dumps(company)
